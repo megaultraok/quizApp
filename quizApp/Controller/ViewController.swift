720167a660a9514c8f8120b4b55f05ddabeb98a9
Jada White <jadaw.2021@gmail.com>
2021-03-08 22:32:33 -0500

    Added score label and logic.

--- quizApp/Controller/ViewController.swift
+++ quizApp/Controller/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var questions: UILabel!
     
+    @IBOutlet weak var score: UILabel!
+    
     @IBOutlet weak var progressBar: UIProgressView!
     
     // Structures or Classes allow you to store two different data types together
@@ -22,6 +24,7 @@ class ViewController: UIViewController {
 //    var questionBank = [["4 + 2 = 7", "False"], ["5 + 3 = 8", "True"], ["1 + 1 = 3", "False"]]
     
     var quizBrain = QuizBrain()
+    var scoreNum = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,16 +38,19 @@ class ViewController: UIViewController {
         trueButton.backgroundColor = UIColor.clear
         falseButton.backgroundColor = UIColor.clear
         progressBar.progress = quizBrain.progressStatus()
+        score.text = String(scoreNum)
         
     }
     
     @IBAction func buttonPressed(_ sender: UIButton) {
+        
         let userInput = sender.currentTitle!
         
         let checkFile = quizBrain.checkAnswer(userInput)
         
         if checkFile {
             sender.backgroundColor = UIColor.green
+            scoreNum += 1
         }
         else {
             sender.backgroundColor = UIColor.red
